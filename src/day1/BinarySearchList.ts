7ed2510af40f22e52bf1916e1751673aeaeac7a1
Thiago Sawada <tms1_tms2@hotmail.com>
2023-08-31 21:00:00 -0300

    feat: binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0;
+  let high = haystack.length;
 
+  while (low < high) {
+    const middle = Math.floor(low + (high - low) / 2);
+    const value = haystack[middle];
+
+    if (needle === value) {
+      return true;
+    } else if (needle > value) {
+      low = middle + 1;
+    } else {
+      high = middle;
+    }
+  }
+
+  return false;
 }
