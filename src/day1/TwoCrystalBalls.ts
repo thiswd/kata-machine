b90fee7be65a19c235537b3693c20c3ad7da984b
Thiago Sawada <tms1_tms2@hotmail.com>
2023-08-31 22:55:06 -0300

    feat: add algorithm for twoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpDistance = Math.floor(Math.sqrt(breaks.length));
+  let breakPoint = -1;
 
+  for (let i = 0; i < breaks.length; i += jumpDistance) {
+    if (breaks[i]) {
+      breakPoint = i;
+      break;
+    }
+  }
+
+  for (let i = breakPoint - jumpDistance + 1; i <= breakPoint; i++) {
+    if (breaks[i]) return i;
+  }
+
+  return -1
 }
