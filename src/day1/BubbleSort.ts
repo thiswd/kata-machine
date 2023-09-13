a8f56833ca27870ee3e6f8689e2aff3a676fe082
Thiago Sawada <tms1_tms2@hotmail.com>
2023-09-12 22:09:31 -0300

    my bubble sort implementation

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
 
+  for (let j = 0; j < arr.length - 1; j++) {
+    for (let i = 0; i < arr.length - j - 1; i++) {
+      const next = i + 1
+
+      if (arr[i] > arr[next]) {
+        [arr[i], arr[next]] = [arr[next], arr[i]]
+      }
+
+    }
+  }
 }
