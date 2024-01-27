7ade8d7ad2d7f3f525a28f3fadd668f0c36d66f0
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-27 16:35:18 -0300

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+  value: T,
+  prev?: Node<T>,
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const newItem = { value: item } as Node<T>;
+    this.length += 1;
+
+    if (!this.head) {
+      this.head = newItem;
+      return;
     }
 
-    push(item: T): void {
+    newItem.prev = this.head;
+    this.head = newItem;
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    if (!this.head) return;
 
-}
-    peek(): T | undefined {
+    this.length -= 1
 
-}
+    const node = this.head
+    this.head = node.prev
+    node.prev = undefined;
+    return node.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
 }
