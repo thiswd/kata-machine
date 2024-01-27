449f37bede9118da9501a5611950ebac094839dd
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-27 16:34:34 -0300

    improve bubble sort performance

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,13 +1,18 @@
 export default function bubble_sort(arr: number[]): void {
 
   for (let j = 0; j < arr.length - 1; j++) {
+    let swapped = false
+
     for (let i = 0; i < arr.length - j - 1; i++) {
       const next = i + 1
 
       if (arr[i] > arr[next]) {
         [arr[i], arr[next]] = [arr[next], arr[i]]
+        swapped = true
       }
 
     }
+
+    if (!swapped) break;
   }
 }
